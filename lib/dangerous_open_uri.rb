19193186c4563911d0142b35e010f4da9cd5c95e
mgi166 <skskoari@gmail.com>
2014-10-16 22:14:06 +0900

    * Overrides OpenURI.open_http

--- lib/dangerous_open_uri.rb
+++ lib/dangerous_open_uri.rb
@@ -1,5 +1,18 @@
 require "dangerous_open_uri/version"
+require 'open-uri'
 
-module DangerousOpenUri
-  # Your code goes here...
+OpenURI.module_eval do
+  instance_eval { alias :original_open_http :open_http }
+
+  def self.open_http(buf, target, proxy, options)
+    if target.userinfo
+      userinfo = target.userinfo
+      user, pass = userinfo.to_s.split(':', -1)
+      options[:http_basic_authentication] = user
+      options[:http_basic_authentication] = pass
+      target.userinfo = ""
+    end
+
+    original_open_http(buf, target, proxy, options)
+  end
 end
