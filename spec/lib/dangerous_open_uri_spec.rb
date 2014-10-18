d83db2fffec8bee1cd303f6efe23d6e60cd8c3d8
mgi166 <skskoari@gmail.com>
2014-10-18 12:06:46 +0900

    * Add new spec `OpenURI.open_http`

--- spec/lib/dangerous_open_uri_spec.rb
+++ spec/lib/dangerous_open_uri_spec.rb
@@ -1,5 +1,32 @@
-
 describe OpenURI do
-  it 'description' do
+  describe '.open_http' do
+    context 'when request with basic authentication' do
+      it 'opens dangerous uri' do
+        stub_request(:any, 'user:pass@www.example.com/secret/page.html')
+          .to_return(body: 'aaa')
+
+        expect(
+          open('http://user:pass@www.example.com/secret/page.html').read
+        ).to eq('aaa')
+      end
+
+      it 'opens dangerous uri(userinfo has two ":")' do
+        stub_request(:any, 'user:pass@www.example.com/secret/page.html')
+          .to_return(body: 'aaa')
+
+        expect(
+          open('http://user:pass:broken@www.example.com/secret/page.html').read
+        ).to eq('aaa')
+      end
+    end
+
+    context 'when request no basic authentication' do
+      it 'opens nomal url' do
+        stub_request(:any, 'www.example.com/index.html').to_return(body: 'aaa')
+        expect(
+          open('http://www.example.com/index.html').read
+        ).to eq('aaa')
+      end
+    end
   end
 end
