6e658bbfceb1c14c61edc23c0f636255908464c9
mgi166 <skskoari@gmail.com>
2014-10-18 12:57:22 +0900

    * Add new test case

--- spec/lib/dangerous_open_uri_spec.rb
+++ spec/lib/dangerous_open_uri_spec.rb
@@ -18,6 +18,39 @@ describe OpenURI do
           open('http://user:pass:broken@www.example.com/secret/page.html').read
         ).to eq('aaa')
       end
+
+      it 'opens dangerous uri(has user but no password)' do
+        stub_request(:any, 'user:@www.example.com/secret/page.html')
+          .to_return(body: 'aaa')
+
+        expect(
+          open('http://user:@www.example.com/secret/page.html').read
+        ).to eq('aaa')
+      end
+
+      it 'opens dangerous uri(no user but has password)' do
+        stub_request(:any, ':pass@www.example.com/secret/page.html')
+          .to_return(body: 'aaa')
+
+        expect(
+          open('http://:pass@www.example.com/secret/page.html').read
+        ).to eq('aaa')
+      end
+
+      it 'opens dangerous uri(userinfo == ":")' do
+        stub_request(:any, 'www.example.com/secret/page.html')
+          .to_return(body: 'aaa')
+
+        expect(
+          open('http://:@www.example.com/secret/page.html').read
+        ).to eq('aaa')
+      end
+
+      it 'raises error(bad uri)' do
+        expect do
+          open('http://@@www.example.com/secret/page.html').read
+        end.to raise_error URI::InvalidURIError
+      end
     end
 
     context 'when request no basic authentication' do
