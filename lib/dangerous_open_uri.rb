f186d3bc84a20a09ab1db7eccbef336c9b467fb7
mgi166 <skskoari@gmail.com>
2014-10-19 09:42:49 +0900

    * if proxy has dangerous uri, so proxy_user, proxy_pass have user, pass from dangerous uri

--- lib/dangerous_open_uri.rb
+++ lib/dangerous_open_uri.rb
@@ -5,6 +5,19 @@ module OpenURI
   instance_eval { alias :original_open_http :open_http }
 
   def self.open_http(buf, target, proxy, options)
+    if proxy
+      proxy_uri, proxy_user, proxy_pass = proxy
+
+      if proxy_uri.userinfo
+        userinfo = proxy_uri.userinfo
+        user, pass = userinfo.to_s.split(':', -1)
+        proxy_user = user
+        proxy_pass = pass
+        proxy_uri.userinfo = ""
+        proxy = [proxy_uri, proxy_user, proxy_pass]
+      end
+    end
+
     if target.userinfo
       userinfo = target.userinfo
       user, pass = userinfo.to_s.split(':', -1)
